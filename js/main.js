d9780bb6e2e0431ac20bbda7bd91c85be3a9b945
Andy Melien <ypm-digitalmedia@gmail.com>
2017-05-23 14:57:40 -0400

    lots of stuff

--- js/main.js
+++ js/main.js
@@ -1,1 +1,36 @@
+$(document).ready(function() {
 
+    $("#mainHeader").fitText(2.0);
+
+    // $('#slider').mouseover(function() {
+    //     $('.carousel-ui').css({ opacity: 0.0, visibility: "visible" }).animate({ opacity: 1.0 });
+    // });
+
+    // $('#slider').mouseout(function() {
+    //     $('.carousel-ui').css({ opacity: 0.0, visibility: "visible" }).animate({ opacity: 0.0 });
+    // });
+
+    // .css({opacity: 0.0, visibility: "visible"}).animate({opacity: 1.0});
+
+    $(".box-item").hover(function() {
+        $(this).toggleClass("flipped");
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
