332838d269aee7638bf1abf3be5ebc4ebd80c24c
Andy Melien <ypm-digitalmedia@gmail.com>
2017-05-24 16:56:36 -0400

    card flipping fixed

--- js/main.js
+++ js/main.js
@@ -12,19 +12,40 @@ $(document).ready(function() {
 
     // .css({opacity: 0.0, visibility: "visible"}).animate({opacity: 1.0});
 
-    $(".box-item").hover(function() {
-        $(this).toggleClass("flipped");
-    })
+    // $(".box-item").hover(_.debounce(function() {
+    //     $(this).toggleClass("flipped");
+    // })
+
+    // var flip = _.debounce(function(el))
+    //     $(el).toggleClass("flipped");
+    // }
+
+    // $(".box-item").hover(function() {
+    //     flip($(this));
+    // });
+
+    $(".box-item").mouseover(function() {
+        flipToBack($(this));
+    });
+
+    $(".box-item").mouseleave(function() {
+        flipToFront($(this));
+    });
+
+    var flip = _.debounce(function(e) {
+        $(e).toggleClass("flipped");
+    }, 100);
+
+    var flipToFront = _.debounce(function(e) {
+        $(e).removeClass("flipped");
+    }, 100);
+
+    var flipToBack = _.debounce(function(e) {
+        $(e).addClass("flipped");
+    }, 100);
 
 
-
-
-
-
-
-
-
-    $(window).resize(function() { console.log($(".box").eq(0).css("width")); });
+    // $(window).resize(function() { console.log($(".box").eq(0).css("width")); });
 
 
 
