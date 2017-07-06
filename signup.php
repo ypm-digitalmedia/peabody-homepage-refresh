cc147b78785956b5b61a98934c1aac7de268d98e
Andy Melien <ypm-digitalmedia@gmail.com>
2017-07-06 11:18:15 -0400

    KZ comments july5

--- signup.php
+++ signup.php
@@ -47,12 +47,11 @@ $headers .= 'From: <no-reply@peabody.yale.edu>' . "\r\n";
 
 $file = 'signups.log';
 // Open the file to get existing content
-// $current = file_get_contents($file);
+$current = file_get_contents($file);
 // Append a new person to the file
 $current .= $dt . "\t" . $arr["firstName"] . " " . $arr["lastName"] . "\t" . $arr["email"] . "\n";
 // Write the contents back to the file
-file_put_contents($file, $current, FILE_APPEND | LOCK_EX);
-
+file_put_contents($file, $current);
 
 mail($to,$subject,$message,$headers);
 
