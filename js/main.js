47f51aadfe1d194891f5fe39fa1e03b1630da0e5
Andy Melien <ypm-digitalmedia@gmail.com>
2017-10-27 13:09:54 -0400

    added november 3 closure alert

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 /* =========================== BEGIN CUSTOMIZATIONS ================================= */
 
-var show_alert = false;
-var show_alert_until = '2017-10-27'; // FORMAT 'YYYY-MM-DD'
+var show_alert = true;
+var show_alert_until = '2017-11-04'; // FORMAT 'YYYY-MM-DD'
 
 var show_evolved_banner = false;
 
